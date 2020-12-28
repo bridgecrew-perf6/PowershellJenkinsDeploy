f0e96291cd08f1b2582a32a89074069f69f1a48e
CORPINDIA\EI11942 <amoonat20@gmail.com>
2020-12-28 14:48:48 +0530

    Added Copy Files File

---
+++ CopyFiles.ps1
@@ -0,0 +1,34 @@
+Function Copy-Files() {
+    [CmdLetBinding()]
+    Param(
+        [Parameter(Mandatory = $true)]
+        $SourcePath,
+        [Parameter(Mandatory = $true)]
+        $DestinationPath
+    )
+    
+    Begin {
+       Write-Host 'Started Function: Executing'
+    }
+
+    Process {
+           
+       $global:foldPath = $null
+       foreach($foldername in $DestinationPath.split("\")) {
+           $global:foldPath += ($foldername+"\")
+           if (!(Test-Path $global:foldPath)){
+               New-Item -ItemType Directory -Path $global:foldPath
+           }
+       }
+       Get-ChildItem -Path $SourcePath | Copy-Item -Destination $DestinationPath -Recurse
+   }
+       
+
+    End {
+    $count = (Get-ChildItem -Path $SourcePath -Recurse).Count
+    Write-Output "Copy Finished! $count Files Copied" 
+    Write-Host 'Terminating Function: Finished Executing'
+    }
+}
+
+Copy-Files -SourcePath "C:\Test\t1" -DestinationPath "C:\Test\t2"
